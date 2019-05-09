26ce53c9f2d04ea850697b43ac086390533eb178
Andi Ndrecka <andi.ndrecka1@gmail.com>
2019-05-09 19:14:45 +0200

    add second business screen

--- src/routes/BusinessTools/BusinessRegistration/BusinessRegistration.js
+++ src/routes/BusinessTools/BusinessRegistration/BusinessRegistration.js
@@ -1,10 +1,23 @@
 import React from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text, Image, TouchableOpacity, TextInput } from 'react-native';
+import ModalDropdown from 'react-native-modal-dropdown';
+
+import { images } from 'theme';
 
 import styles from './styles';
 
+const LineInput = ({ label }) => (
+  <View style={styles.lineInputContainer}>
+    <Text style={styles.inputLabel}>{label}</Text>
+
+    <TextInput style={styles.lineInput} />
+  </View>
+);
+
 class BusinessRegistration extends React.Component {
   render() {
+    const { navigation } = this.props;
+
     return (
       <View style={styles.container}>
         <Text style={styles.notice}>
@@ -12,7 +25,38 @@ class BusinessRegistration extends React.Component {
           to share your stuff!
         </Text>
 
-        <TouchableOpacity style={styles.button}>
+        <View style={styles.subContainer}>
+          <Image source={images.logo} style={styles.logoIcon} />
+
+          <TextInput placeholder="Business Name Here" style={styles.nameInput} />
+        </View>
+
+        <View style={styles.subContainer}>
+          <LineInput label="Street Address" />
+          <LineInput label="City" />
+          <LineInput label="Zip Code" />
+        </View>
+
+        <View style={styles.row}>
+          <Text style={styles.inputLabel}>Choose Category</Text>
+
+          <ModalDropdown
+            options={['Restaurant', 'Bar', 'Other']}
+            style={styles.modalDropdown}
+            dropdownStyle={styles.dropdownStyle}
+            renderRow={(option, index) => (
+              <View style={styles.dropdownRow}>
+                <Text style={[styles.dropdownRowText, index === 2 && { marginBottom: 0 }]}>
+                  {option}
+                </Text>
+              </View>
+            )}
+            renderSeparator={() => null}
+            adjustFrame={style => ({ ...style, top: style.top + 10, right: style.right - 20 })}
+          />
+        </View>
+
+        <TouchableOpacity style={styles.button} onPress={() => navigation.popToTop()}>
           <Text style={styles.buttonText}>Complete</Text>
         </TouchableOpacity>
       </View>
--- src/routes/BusinessTools/BusinessRegistration/styles.js
+++ src/routes/BusinessTools/BusinessRegistration/styles.js
@@ -5,6 +5,12 @@ import { fonts } from 'theme';
 export default StyleSheet.create({
   container: {
     flex: 1,
+    alignItems: 'center',
+    justifyContent: 'space-around'
+  },
+
+  subContainer: {
+    width: '100%',
     alignItems: 'center'
   },
 
@@ -12,6 +18,85 @@ export default StyleSheet.create({
     width: '80%',
     textAlign: 'center',
     fontSize: 14,
+    color: 'black',
+    fontFamily: fonts.AvenirNextMedium
+  },
+
+  logoIcon: {
+    height: 38,
+    width: 35,
+    resizeMode: 'contain',
+    marginBottom: 5
+  },
+
+  nameInput: {
+    width: '60%',
+    height: 40,
+    borderRadius: 20,
+    backgroundColor: '#F1F1F1C0',
+    fontSize: 14,
+    color: '#3B3B3B',
+    fontFamily: fonts.AvenirNextMedium,
+    alignItems: 'center',
+    justifyContent: 'center',
+    textAlign: 'center'
+  },
+
+  row: {
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+
+  lineInputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginVertical: 7
+  },
+
+  inputLabel: {
+    fontSize: 14,
+    color: 'black',
+    fontFamily: fonts.AvenirNextMedium,
+    width: '40%',
+    textAlign: 'right',
+    marginRight: 20
+  },
+
+  lineInput: {
+    width: '40%',
+    marginRight: '20%',
+    paddingVertical: 3,
+    borderBottomWidth: 1,
+    borderColor: '#00000080'
+  },
+
+  modalDropdown: {
+    width: '40%',
+    height: 30,
+    marginRight: '20%',
+    backgroundColor: '#F1F1F1',
+    borderRadius: 5,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+
+  dropdownStyle: {
+    height: 66,
+    width: 130,
+    borderWidth: 0
+  },
+
+  dropdownRow: {
+    height: 20,
+    borderWidth: StyleSheet.hairlineWidth,
+    borderColor: '#00000080',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 3
+  },
+
+  dropdownRowText: {
+    fontSize: 12,
     color: 'black',
     fontFamily: fonts.AvenirNextMedium
   },
