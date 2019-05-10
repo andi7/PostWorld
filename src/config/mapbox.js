cfc8c9c052a33d1821f7ab1a962aca5bb3ae268f
Andi Ndrecka <andi.ndrecka1@gmail.com>
2019-05-11 01:14:37 +0200

    fix style duplicate

--- src/config/mapbox.js
+++ src/config/mapbox.js
@@ -1,6 +1,5 @@
 export default {
   mapAccessToken:
     'pk.eyJ1IjoiYWJkaTQyIiwiYSI6ImNqaHNlZm9pYTAyM3kzcW15Y2kzdHd3N2kifQ.899OIbr__amO23qmSrRmyw',
-  mapStyleUrl:
-    'mapbox://styles/abdi42/cjhsge0rx7vyo2rp9kfssubsz?optimize=truemapbox://styles/abdi42/cjhsge0rx7vyo2rp9kfssubsz?optimize=true'
+  mapStyleUrl: 'mapbox://styles/abdi42/cjhsge0rx7vyo2rp9kfssubsz?optimize=true'
 };
